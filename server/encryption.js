2ffc45979a592fd6dcd2199c19d8d41426081fb0
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-27 21:20:16 +0700

    adding compare password function

--- server/encryption.js
+++ server/encryption.js
@@ -4,3 +4,7 @@ export const hashPassword = async (password) => {
   var salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
+
+export const comparePassword = async (passwordA, passwordB) => {
+  return await bcrypt.compare(passwordA, passwordB);
+};
