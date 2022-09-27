a8fd29b07fb28aecfa762d700861d4af6e895f51
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-27 21:21:01 +0700

    adding get, delete, sub, unsub functionallity

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -33,36 +33,71 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  res.json({
-    message: "Successfull",
-  });
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndRemove(req.params.id);
+      res.status(204);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "Invalid permission"));
+  }
 };
 
 export const getUser = async (req, res, next) => {
-  res.json({
-    message: "Successfull",
-  });
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const subscribe = async (req, res, next) => {
-  res.json({
-    message: "Successfull",
-  });
+  try {
+    await User.findById(req.user.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: {
+        subscribers: 1,
+      },
+    });
+    res.status(200).json("Subscription successfull");
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const unsubscribe = async (req, res, next) => {
-  res.json({
-    message: "Successfull",
-  });
+  try {
+    await User.findById(req.user.id, {
+      $pull: { subscribedUsers: req.params.id },
+    });
+
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: {
+        subscribers: -1,
+      },
+    });
+    res.status(200).json("Unsubscription successfull");
+  } catch (err) {
+    next(err);
+  }
 };
+
 export const like = async (req, res, next) => {
-  res.json({
-    message: "Successfull",
-  });
+  try {
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const dislike = async (req, res, next) => {
-  res.json({
-    message: "Successfull",
-  });
+  try {
+  } catch (err) {
+    next(err);
+  }
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -13,11 +13,11 @@ import { verifyToken } from "../verifyToken.js";
 const router = express.Router();
 
 router.put("/:id", verifyToken, updateUser);
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyToken, deleteUser);
 router.get("/:id", getUser);
-router.put("/sub/:channelId", subscribe);
-router.put("/unsub/:channelId", unsubscribe);
-router.put("/like/:videoId", like);
-router.put("/dislike/:videoId", dislike);
+router.put("/sub/:channelId", verifyToken, subscribe);
+router.put("/unsub/:channelId", verifyToken, unsubscribe);
+router.put("/like/:videoId", verifyToken, like);
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
