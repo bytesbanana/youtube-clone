987de567ebdc35ddb9d4f463153d7e57dd9d4c06
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-29 19:04:24 +0700

    update video route

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,8 +1,25 @@
-import express from 'express';
-// import { test } from '../controllers/video.js';
+import express from "express";
+import {
+  addVideo,
+  addView,
+  deleteVideo,
+  getVideo,
+  random,
+  sub,
+  trend,
+  updateVideo,
+} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-// router.get('/test', test);
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, updateVideo);
+router.delete("/", verifyToken, deleteVideo);
+router.get("/:id", getVideo);
+router.put("/view/:id", addView);
+router.put("/trend", trend);
+router.put("/random", random);
+router.put("/sub", verifyToken, sub);
 
 export default router;
